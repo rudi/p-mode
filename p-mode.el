b24fc7d9c2c4c8bdac4d6f6ef8399e0b4c1358b0
Rudi Schlatte <rudi@constantly.at>
2023-01-19 15:32:48 +0100

    Add first version of running pc, pmc from buffer
    Todos:
    - do not hardcode program names (pmc, coyote)
    - maybe present file names in a nicer way instead of via full paths
    - add more tools, commandline options as we discover them

--- p-mode.el
+++ p-mode.el
@@ -5,7 +5,7 @@
 ;; Author: Rudolf Schlatte <rudi@constantly.at>
 ;; URL: https://github.com/rudi/p-mode
 ;; Version: 0.1
-;; Package-Requires: ((emacs "28.1") (yasnippet "0.14.0"))
+;; Package-Requires: ((emacs "28.1") (yasnippet "0.14.0") (transient "0.3"))
 ;; Keywords: tools, languages
 
 ;; This program is free software; you can redistribute it and/or modify
@@ -29,11 +29,18 @@
 ;;; Code:
 
 (require 'yasnippet)
+(require 'transient)
+(require 'cl-lib)
 
-;; Customizations
+;;; Customizations
 (defgroup p nil
   "Major mode for editing files for the P model checker."
   :group 'languages)
+
+(defcustom p-pc-command "pc"
+  "The command to run the P model checker."
+  :type 'string
+  :risky t)
 
 (defcustom p-override-pascal-file-type nil
   "Toggle to choose whether to associate the `.p' file extension with `p-mode'.
@@ -45,6 +52,7 @@ By default, `.p' is associated with files written in the Pascal language."
   :type 'hook
   :options (list 'yas-minor-mode-on))
 
+;;; Snippets
 (defvar p--yas-snippets-dir
   (expand-file-name
    "snippets"
@@ -59,7 +67,7 @@ By default, `.p' is associated with files written in the Pascal language."
       (buffer-file-name)))))
   "Directory containing yasnippet snippets for P.")
 
-;; Syntax highlighting
+;;; Syntax highlighting
 (defvar p-mode-syntax-table (copy-syntax-table)
   "Syntax table for `p-mode'.")
 (modify-syntax-entry ?/   ". 124" p-mode-syntax-table)
@@ -129,6 +137,181 @@ By default, `.p' is associated with files written in the Pascal language."
    ())
   "Font lock information for P.")
 
+;;; Support for running various tools
+(defvar p--last-tool-run nil
+  "Identifies the last tool run on the current model (compiler, checker, ...).
+This is used to decide how to parse the `*p-compilation*' buffer
+after the tool has finished.")
+
+(defvar p--compiled-model-dlls nil
+  "List of DLLs produced by the last successful run of `pc'.")
+
+(defvar p--compiled-model-names nil
+  "List of model names produced by the last successful run of `pc'.")
+
+(define-compilation-mode p-compilation-mode "P-compile"
+  "A variant of `compilation-mode' used for running P tools."
+  (add-hook 'p-compilation-finish-functions
+            'p--analyze-tool-output))
+
+(defun p--analyze-tool-output (buffer status)
+  "Extract various state information from output of P tools.
+Argument BUFFER is the compilation buffer, STATUS is the status;
+\"finished\" for successful compilation."
+  ;; After `pc', get the name(s) of DLLs containing the compiled models
+  ;;   ClientServer -> /Users/rudi/Sync/Source/P/P/Tutorial/1_ClientServer/POutput/netcoreapp3.1/ClientServer.dll
+  (cl-case p--last-tool-run
+    (:pc
+     (setq p--compiled-model-dlls nil
+           p--compiled-model-names nil)
+     (save-match-data
+       (with-current-buffer buffer
+         (goto-char (point-min))
+         (while (re-search-forward "^\\s-+\\([a-zA-Z0-9_]+\\) -> \\(.+\\.dll\\)$" (point-max) t)
+           (let ((model-name (match-string-no-properties 1))
+                 (dll-name (match-string-no-properties 2)))
+             (when (file-exists-p dll-name)
+               (push model-name p--compiled-model-names)
+               (push dll-name p--compiled-model-dlls)))
+           (goto-char (match-end 0)))))
+     (setq p--compiled-model-dlls (nreverse p--compiled-model-dlls)
+           p--compiled-model-names (nreverse p--compiled-model-names))
+     (unless (member p--current-model-dll p--compiled-model-dlls)
+       ;; convenience: set dll to something meaningful, or nil if we didn't
+       ;; create any dll
+       (setq p--current-model-dll (car p--compiled-model-dlls))))
+    (:pmc
+     (message "P model checker finished successfully."))))
+
+(defun p--test-cases-from-dll (dll)
+  "List all test cases in DLL.
+This parses the output of `pmc <dll>'."
+  ;; todo use `with-temp-buffer'
+  (let ((test-cases nil))
+    (with-current-buffer (get-buffer-create "*coyote output*")
+      (erase-buffer)
+      (call-process "coyote" nil t nil "test" dll)
+      (goto-char (point-min))
+      (while (re-search-forward "^[a-zA-Z0-9_.]+$" (point-max) t)
+        (goto-char (match-end 0))
+        (push (match-string-no-properties 0) test-cases)))
+    test-cases))
+
+(defun p--run-pc-compiler (&optional _args)
+  "Run the compiler."
+  (interactive
+   (list (transient-args 'p-transient)))
+  (transient-set)
+  (setq-local compile-command
+              (concat p-pc-command " "
+                      "-proj:" (expand-file-name p--current-project-file)))
+  (setq p--last-tool-run :pc)
+  (compilation-start compile-command 'p-compilation-mode))
+
+(defun p--run-pmc (&optional _args)
+  "Run the model checker."
+  (interactive
+   (list (transient-args 'p-transient)))
+  (transient-set)
+  (setq p--last-tool-run :pmc)
+  (setq-local compile-command
+              (concat "coyote" " " "test" " "
+                      (expand-file-name p--current-model-dll)
+                      " " "-m" " " p--current-test-case-from-dll))
+  (setq p--last-tool-run :pmc)
+  (compilation-start compile-command 'p-compilation-mode)
+  ;; prompt user for dll; use content of `p--compiled-model-dlls' for
+  ;; suggestions, maybe introduce `p--last-checked-dll' as default
+
+  ;; list test cases in chosen dll via the output of `coyote test foo.dll';
+  ;; prompt user for test to run as value of `-m' parameter
+
+  ;; prompt user for number of schedules as value of `-i' parameter
+  )
+
+;;; Transient menus
+(defvar-local p--current-project-file nil
+  "The project file to use for the `-proj:' argument to pc.")
+
+(defvar-local p--current-model-dll nil
+  "The model dll to check for pmc.")
+
+(defvar-local p--current-test-case-from-dll nil
+  "The test case to use for the `-m' argument to pmc.")
+
+(defvar-local p--number-of-schedules 1
+  "The number of schedules to use for the `-i' argument to pmc.")
+
+(transient-define-infix p--project-file (&optional _args)
+  :description "Project file"
+  :class 'transient-lisp-variable
+  :variable 'p--current-project-file
+  :key "-p"
+  :argument "-proj:"
+  :reader (lambda (prompt _initial-input _history)
+            (read-file-name
+             prompt
+             (file-name-directory (or p--current-project-file ""))
+             (file-name-nondirectory (or p--current-project-file ""))
+             t
+             nil)))
+
+(transient-define-infix p--program-dll (&optional _args)
+  :description "Compiled dll"
+  :class 'transient-lisp-variable
+  :variable 'p--current-model-dll
+  :key "d"
+  :reader (lambda (prompt _initial-input _history)
+            ()
+            (read-file-name
+             prompt
+             (file-name-directory (or p--current-model-dll ""))
+             (file-name-nondirectory (or p--current-model-dll ""))
+             t
+             nil)))
+
+(transient-define-infix p--test-case-from-dll (&optional _args)
+  :description "Test case"
+  :class 'transient-lisp-variable
+  :variable 'p--current-test-case-from-dll
+  :key "-m"
+  :reader (lambda (prompt initial-input history)
+            (completing-read prompt
+                             (p--test-cases-from-dll p--current-model-dll)
+                             nil t initial-input history)))
+
+(defun p--read-integer (prompt initial-input history)
+  "Read an integer from the minibuffer, prompting with PROMPT.
+INITIAL-INPUT is the default value, HISTORY is the input history."
+  (let ((s nil))
+    (while (progn
+             (setq s (read-from-minibuffer prompt initial-input nil nil history))
+             (unless (integerp (setq s (car (ignore-errors (read-from-string s)))))
+               (message "Please enter a whole number.")
+               (sit-for 1)
+               t)))
+    s))
+
+(transient-define-infix p--test-case-schedules (&optional _args)
+  :description "Number of schedules"
+  :class 'transient-lisp-variable
+  :variable 'p--number-of-schedules
+  :key "-i"
+  :reader #'p--read-integer)
+
+(transient-define-prefix p-transient ()
+  "Menu of commands for P files."
+  ["Compile"
+   (p--project-file)
+   ("c" "Compile model" p--run-pc-compiler)]
+  ["Test"
+   (p--program-dll)
+   (p--test-case-from-dll)
+   (p--test-case-schedules)
+   ("t" "Test model" p--run-pmc)])
+
+;;; The major mode itself
+
 ;;;###autoload
 (define-derived-mode p-mode prog-mode "P"
   "Major mode for editing files for the P model checker."
@@ -150,6 +333,8 @@ By default, `.p' is associated with files written in the Pascal language."
 ;; NOTE: Adding ourselves to `auto-mode-alist' would be a bit unfriendly since
 ;; it shadows the file type association for the existing Pascal mode; make
 ;; this a user option for now.
+
+(define-key p-mode-map (kbd "C-c C-c") 'p-transient)
 
 ;;;###autoload
 (when p-override-pascal-file-type
